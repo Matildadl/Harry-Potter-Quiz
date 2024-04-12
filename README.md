021e7abed0563fb4cd5d68a3391590ad7d754909
Matilda Danas-Leek <mfragetecken@gmail.com>
2024-04-12 19:24:15 +0200

    added more questions and css for correct and incorrect answer

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 link to image: https://harrypotter.fandom.com/sv/wiki/Stora_Salen
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,73 +1,116 @@
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
-
-let shuffledQuestions, currentQuestionsIndex
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 
+let shuffledQuestions, currentQuestionIndex
+
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
 
 function startGame() {
-    console.log('Started')
-    startButton.classList.add('hide')
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionsIndex = 0
-    questionContainerElement.classList.remove('hide')
-    setNextQuestion
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
 }
 
 function setNextQuestion() {
-    resetState()
-    showQuestion(shuffledQuestions[currentQuestionsIndex])
-
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function showQuestion(question){
-    questionElement.innertext = question.question
-    question.answers.ForEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
 }
 
 function resetState() {
-    nextButton.classlist.add('hide')
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild
-        (answerButtonsElement.firstChild)
-    }
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
 }
-function selectAnswer (e) {
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button =>
-    setStatusClass(button. button.dataset.correct))
 
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
 }
+
 function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classlist.add('correct')
-    } else {
-        element.classlist.add('wrong')
-    }
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
 }
 
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
 
 const questions = [
-    {
-        question: 'what is 2 + 2?',
-        answers: [
-            { text: '4', correct: true },
-            { text: '22', correct: false }
-        ]
-    }
+  {
+    question: 'Who is Harry Potter?',
+    answers: [
+      { text: 'The most famous Wizard on earth', correct: true },
+      { text: 'Voldemort', correct: false },
+      { text: 'A dog', correct: false },
+      { text: 'A cat'}
+    ]
+  },
+  {
+    question: 'What is the name of Harry Potters School?',
+    answers: [
+      { text: 'Gringotts', correct: false },
+      { text: 'Hogwarts', correct: true },
+      { text: 'Slytherin', correct: false },
+      { text: 'Azkaban', correct: false }
+    ]
+  },
+  {
+    question: 'Who killed Harrys parents?',
+    answers: [
+      { text: 'Prof. Dumbledore', correct: false },
+      { text: 'Madame Pomfrey', correct: false },
+      { text: 'Lord Voldemort', correct: true },
+      { text: 'Prof. Snape', correct: false }
+    ]
+  },
+  {
+    question: 'What kind of animal does Harry Potter have?',
+    answers: [
+      { text: 'A Cat', correct: false },
+      { text: 'A Dog', correct: false },
+      { text: 'An Owl', correct: true },
+      { text: 'A Rabbit', correct: false }
+    ]
+  }
 ]
