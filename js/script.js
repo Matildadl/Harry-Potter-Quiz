2d5f76f1e50f85d5bb2bf5c795c3d70e848833af
Matilda Danas-Leek <mfragetecken@gmail.com>
2024-04-17 15:11:03 +0200

    changed the responsive for iphone

--- js/script.js
+++ js/script.js
@@ -1,153 +1,181 @@
-const startButton = document.getElementById('start-btn');
-const nextButton = document.getElementById('next-btn');
-const questionContainerElement = document.getElementById('question-container');
-const questionElement = document.getElementById('question');
-const answerButtonsElement = document.getElementById('answer-buttons');
-const resultButton = document.querySelector('.result-button');
-const resultElement = document.getElementById('result');
-const summaryElement = document.getElementById('summary');
+document.addEventListener('DOMContentLoaded', function() {
+    // Hämta referenser till alla element vi behöver använda
+    const elements = {
+        startButton: document.getElementById('start-btn'),
+        nextButton: document.getElementById('next-btn'),
+        questionContainerElement: document.getElementById('question-container'),
+        questionElement: document.getElementById('question'),
+        answerButtonsElement: document.getElementById('answer-buttons'),
+        resultButton: document.querySelector('.result-button'),
+        resultElement: document.getElementById('result'),
+        summaryElement: document.getElementById('summary'),
+        
+    };
 
+    // Variabler för frågespel
+    let shuffledQuestions, currentQuestionIndex, correctAnswers, totalQuestions;
 
-let shuffledQuestions, currentQuestionIndex, correctAnswers, totalQuestions;
-let highScore = localStorage.getItem('highScore') || 0;
-let gamesPlayed = parseInt(localStorage.getItem('gamesPlayed')) || 0;
+    // Variabler för high score och antal spelade spel
+    let highScore = localStorage.getItem('highScore') || 0;
+    let gamesPlayed = parseInt(localStorage.getItem('gamesPlayed')) || 0;
 
-startButton.addEventListener('click', startGame);
-nextButton.addEventListener('click', () => {
-    currentQuestionIndex++;
-    setNextQuestion();
-});
-resultButton.addEventListener('click', showResults);
-
-function startGame() {
-    const gamesPlayedElement = document.getElementById('games-played');
-    gamesPlayedElement.innerText = gamesPlayed;
-    gamesPlayed++;
-    localStorage.setItem('gamesPlayed', gamesPlayed);
-    startButton.classList.add('hide');
-    resultButton.classList.add('hide');
-    shuffledQuestions = questions.sort(() => Math.random() - 0.5);
-    currentQuestionIndex = 0;
-    correctAnswers = 0;
-    totalQuestions = questions.length;
-    questionContainerElement.classList.remove('hide');
-    resultElement.innerText = '';
-    summaryElement.innerText = '';
-    resetState();
-    setNextQuestion();
-}
-
-function setNextQuestion() {
-    resetState();
-    showQuestionWithTransition(shuffledQuestions[currentQuestionIndex]);
-}
-
-function showQuestionWithTransition(question) {
-    const questionContainer = document.getElementById('question-container');
-    questionContainer.style.opacity = '0';
-    setTimeout(() => {
-        questionElement.innerText = question.question;
-        question.answers.forEach(answer => {
-            const button = document.createElement('button');
-            button.innerText = answer.text;
-            button.classList.add('btn');
-            if (answer.correct) {
-                button.dataset.correct = answer.correct;
-            }
-            button.addEventListener('click', selectAnswer);
-            answerButtonsElement.appendChild(button);
-        });
-        questionContainer.style.opacity = '1';
-    }, 300);
-}
-
-function resetState() {
-    clearStatusClass(document.body);
-    nextButton.classList.add('hide');
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
-    }
-}
-
-function selectAnswer(e) {
-    const selectedButton = e.target;
-    const correct = selectedButton.dataset.correct;
-    if (correct) {
-        correctAnswers++;
-    }
-    setStatusClass(document.body, correct);
-    Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct);
+    // Lägg till händelselyssnare för knappar och modalen
+    elements.startButton.addEventListener('click', startGame);
+    elements.nextButton.addEventListener('click', () => {
+        currentQuestionIndex++;
+        setNextQuestion();
     });
-    if (shuffledQuestions.length > currentQuestionIndex + 1) {
-        nextButton.classList.remove('hide');
-    } else {
-        resultButton.classList.remove('hide');
+    elements.resultButton.addEventListener('click', showResults);
+    
+    // Funktion för att starta spelet
+    function startGame() {
+        const gamesPlayedElement = document.getElementById('games-played');
+        gamesPlayedElement.innerText = gamesPlayed;
+        gamesPlayed++;
+        localStorage.setItem('gamesPlayed', gamesPlayed);
+
+        // Dölj startknappen och resultatkknappen
+        elements.startButton.classList.add('hide');
+        elements.resultButton.classList.add('hide');
+
+        // Blanda frågorna och återställ index och antal korrekta svar
+        shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+        currentQuestionIndex = 0;
+        correctAnswers = 0;
+        totalQuestions = questions.length;
+
+        // Visa frågecontainer och rensa tidigare resultat
+        elements.questionContainerElement.classList.remove('hide');
+        elements.resultElement.innerText = '';
+        elements.summaryElement.innerText = '';
+
+        // Återställ frågestatus och visa nästa fråga
+        resetState();
+        setNextQuestion();
     }
-    updateSummary();
-}
 
-function setStatusClass(element, correct) {
-    clearStatusClass(element);
-    if (correct) {
-        element.classList.add('correct');
-    } else {
-        element.classList.add('wrong');
+    
+    // Funktion för att visa nästa fråga
+    function setNextQuestion() {
+        resetState();
+        showQuestionWithTransition(shuffledQuestions[currentQuestionIndex]);
     }
-}
 
-function clearStatusClass(element) {
-    element.classList.remove('correct');
-    element.classList.remove('wrong');
-}
+    // Funktion för att visa fråga med övergångseffekt
+    function showQuestionWithTransition(question) {
+        const questionContainer = document.getElementById('question-container');
+        questionContainer.style.opacity = '0';
+        setTimeout(() => {
+            elements.questionElement.innerText = question.question;
+            question.answers.forEach(answer => {
+                const button = document.createElement('button');
+                button.innerText = answer.text;
+                button.classList.add('btn');
+                if (answer.correct) {
+                    button.dataset.correct = answer.correct;
+                }
+                button.addEventListener('click', selectAnswer);
+                elements.answerButtonsElement.appendChild(button);
+            });
+            questionContainer.style.opacity = '1';
+        }, 300);
+    }
 
-function showResults() {
-    startButton.innerText = 'Restart';
-    startButton.classList.remove('hide');
-    resultElement.innerText = `Number of correct answers: ${correctAnswers} / ${totalQuestions}`;
-    resultElement.classList.remove('hide');
-    endQuiz(); 
-    displayHighScore(); 
-}
+    // Funktion för att återställa spelstatus
+    function resetState() {
+        clearStatusClass(document.body);
+        elements.nextButton.classList.add('hide');
+        while (elements.answerButtonsElement.firstChild) {
+            elements.answerButtonsElement.removeChild(elements.answerButtonsElement.firstChild);
+        }
+    }
 
-function updateSummary() {
-    summaryElement.innerText = `Number of correct answers: ${correctAnswers} / ${totalQuestions}`;
-    resultElement.classList.remove('hide');
-}
+    // Funktion för att hantera svarsklick
+    function selectAnswer(e) {
+        const selectedButton = e.target;
+        const correct = selectedButton.dataset.correct;
+        if (correct) {
+            correctAnswers++;
+        }
+        setStatusClass(document.body, correct);
+        Array.from(elements.answerButtonsElement.children).forEach(button => {
+            setStatusClass(button, button.dataset.correct);
+        });
+        if (shuffledQuestions.length > currentQuestionIndex + 1) {
+            elements.nextButton.classList.remove('hide');
+        } else {
+            elements.resultButton.classList.remove('hide');
+        }
+        updateSummary();
+    }
 
-function showHighScoreContainer() {
-    const highScoreContainer = document.getElementById('high-score-container');
-    highScoreContainer.classList.remove('hide');
-}
+    // Funktion för att ange klass för svarstatus
+    function setStatusClass(element, correct) {
+        clearStatusClass(element);
+        if (correct) {
+            element.classList.add('correct');
+        } else {
+            element.classList.add('wrong');
+        }
+    }
 
+    // Funktion för att ta bort klass för svarstatus
+    function clearStatusClass(element) {
+        element.classList.remove('correct');
+        element.classList.remove('wrong');
+    }
 
-function updateHighScore(score) {
-    const currentHighScore = parseInt(localStorage.getItem('highScore'), 10) || 0;
-    if (score > currentHighScore) {
-        localStorage.setItem('highScore', score);
+    // Funktion för att visa resultat efter att alla frågor har besvarats
+    function showResults() {
+        elements.startButton.innerText = 'Restart';
+        elements.startButton.classList.remove('hide');
+        elements.resultElement.innerText = `Number of correct answers: ${correctAnswers} / ${totalQuestions}`;
+        elements.resultElement.classList.remove('hide');
+        endQuiz();
         displayHighScore();
     }
-    
-}
 
-function displayHighScore() {
-    const highScore = localStorage.getItem('highScore');
-    document.getElementById('high-score').innerText = `High Score: ${highScore}`;
-    showHighScoreContainer(); 
-    console.log(highScore);
-}
+    // Funktion för att uppdatera summeringsinformationen
+    function updateSummary() {
+        elements.summaryElement.innerText = `Number of correct answers: ${correctAnswers} / ${totalQuestions}`;
+        elements.resultElement.classList.remove('hide');
+    }
 
-function endQuiz() {
-    const score = calculateScore(); 
-    updateHighScore(score);
+    // Funktion för att visa behållaren för high score
+    function showHighScoreContainer() {
+        const highScoreContainer = document.getElementById('high-score-container');
+        highScoreContainer.classList.remove('hide');
+    }
+
+    // Funktion för att uppdatera high score
+    function updateHighScore(score) {
+        const currentHighScore = parseInt(localStorage.getItem('highScore'), 10) || 0;
+        if (score > currentHighScore) {
+            localStorage.setItem('highScore', score);
+            displayHighScore();
+        }
+    }
+
+    // Funktion för att visa high score
+    function displayHighScore() {
+        const highScore = localStorage.getItem('highScore');
+        document.getElementById('high-score').innerText = `High Score: ${highScore}`;
+        showHighScoreContainer();
+        console.log(highScore);
+    }
+
+    // Funktion för att avsluta frågespelet
+    function endQuiz() {
+        const score = calculateScore();
+        updateHighScore(score);
+        displayHighScore();
+    }
+
+    // Funktion för att beräkna poängen
+    function calculateScore() {
+        return correctAnswers;
+    }
+
+    // Anropa displayHighScore() när sidan laddas för att visa high score från början
     displayHighScore();
-}
-
-
-function calculateScore() {
-    // Returnera antalet korrekta svar
-    return correctAnswers;
-}
-
-// Anropa displayHighScore() när sidan laddas för att visa high-score från början
-displayHighScore();
+});
